5aab2d5d3a3a218918701a29e0f889bd7f179b8f
Zachary Fleck <87536651+zacharyfleck@users.noreply.github.com>
2024-02-25 22:19:35 -0500

    swith fontawesome from in-line to css import (#6)

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { NgIf } from '@angular/common';
+
+import { config } from '@fortawesome/fontawesome-svg-core';
 import { faFacebook, faInstagram, faTiktok, faThreads, faYoutube, faDiscord } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+config.autoAddCss = false;
 
 import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
 import { MatButton } from '@angular/material/button';
